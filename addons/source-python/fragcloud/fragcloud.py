f86ca7d7d94ffd03507153c6e92745814db076b0
kamikazekuh <33634496+kamikazekuh@users.noreply.github.com>
2020-12-22 14:09:27 +0100

    Added fire and light effect to smg grenade and frag

--- addons/source-python/fragcloud/fragcloud.py
+++ addons/source-python/fragcloud/fragcloud.py
@@ -1,11 +1,20 @@
-from colors import Color
+from colors import Color,RED
 from entities.entity import Entity
 from events import Event
+from listeners import OnEntityDeleted
 from messages import Shake, Fade
 from players.entity import Player
+from stringtables import string_tables
+
 
 fade_color = Color(255, 11, 11)
 fade_message = Fade(duration=1, hold_time=1, color=fade_color.with_alpha(150))
+
+@OnEntityDeleted
+def on_entity_deleted(base_entity):
+    if base_entity.classname in ['npc_grenade_frag','grenade_ar2']:
+        create_particle(base_entity.origin, "fire_large_01",5.0)
+        create_light(base_entity.origin,5.0)
 
 @Event('player_death')
 def player_death(ev):
@@ -47,3 +56,27 @@ def player_death(ev):
         ar2.spawn()
         ar2.call_input("Explode")
         ar2.call_input("Kill")
+        
+        
+def create_particle(position,particle_name,duration):
+    entity = Entity.create("info_particle_system")
+    entity.effect_name = particle_name
+    entity.effect_index = string_tables.ParticleEffectNames.add_string(particle_name)
+    entity.origin = position
+    entity.start()
+    entity.delay(duration,entity.call_input,("Kill",))
+    
+def create_light(position,duration):
+    entity = Entity.create("light_dynamic")
+    entity.inner_cone = 0
+    entity.cone = 80
+    entity.brightness = 1
+    entity.spotlight_radius = 240.0
+    entity.distance = 300.0
+    entity.color = Color(255,100,10)
+    entity.pitch = -90
+    entity.style = 5
+    entity.origin = position
+    entity.spawn()
+    entity.call_input("TurnOn")
+    entity.delay(duration,entity.call_input,("Kill",))
